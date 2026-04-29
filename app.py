259c4c906f3c7e63faafcd6c08f0455e47ca49f2
khantoucheiskander748-lgtm <khantoucheiskander748@gmail.com>
2026-04-29 05:25:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-import os
 from flask import Flask, jsonify
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-
-# قراءة المتغيرات من Environment Variables
-TELEGRAM_API_ID = int(os.getenv("TELEGRAM_API_ID"))
-TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHANNEL = os.getenv("TELEGRAM_CHANNEL")
-
-QUOTEX_EMAIL = os.getenv("QUOTEX_EMAIL")
-QUOTEX_COOKIES = os.getenv("QUOTEX_COOKIES")
-QUOTEX_TOKEN = os.getenv("QUOTEX_TOKEN")
-QUOTEX_USER_AGENT = os.getenv("QUOTEX_USER_AGENT")
+CORS(app)  # السماح لجميع المواقع بالوصول
 
 @app.route("/")
 def home():
     return jsonify({
+        "quotex_email": os.getenv("QUOTEX_EMAIL"),
         "status": "running",
-        "telegram_api_id": TELEGRAM_API_ID,
-        "telegram_api_hash": TELEGRAM_API_HASH,
-        "telegram_channel": TELEGRAM_CHANNEL,
-        "quotex_email": QUOTEX_EMAIL
+        "telegram_api_hash": os.getenv("TELEGRAM_API_HASH"),
+        "telegram_api_id": int(os.getenv("TELEGRAM_API_ID")),
+        "telegram_channel": os.getenv("TELEGRAM_CHANNEL")
     })
